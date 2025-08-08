dd0b98ed2b916847a7fa5e792723ffb71c8999fc
mrazem <mrazem@student.42heilbronn.de>
2025-08-08 18:45:14 +0200

    Makefile and basic testing structure

---
+++ include/libft/ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrazem <mrazem@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/13 20:38:45 by mrazem            #+#    #+#             */
+/*   Updated: 2025/03/22 13:57:48 by mrazem           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//copy n bytes from memory src to memory dst
+//
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!dst && !src && n > 0)
+		return (NULL);
+	while (i < n)
+	{
+		*(unsigned char *)(dst + i) = *(unsigned char *)(src + i);
+		i++;
+	}
+	return (dst);
+}
